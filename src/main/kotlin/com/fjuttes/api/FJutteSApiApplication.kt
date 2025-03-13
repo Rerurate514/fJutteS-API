989d9fd80f8ae976355b56f7646dcc1112aca8a2
Rerurate514 <rerurateyuto@gmail.com>
2025-03-13 18:36:30 +0900

    first commit

---
+++ src/main/kotlin/com/fjuttes/api/FJutteSApiApplication.kt
@@ -0,0 +1,11 @@
+package com.fjuttes.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FJutteSApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FJutteSApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/fjuttes/api/FJutteSApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fjuttes.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FJutteSApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
